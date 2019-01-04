938ea297c648653882b7d429e6f87a94139e5e57
kshitij0809 <kshitijpratap0809@gmail.com>
2019-01-04 16:32:36 +0530

    added the get contact by id

--- src/server/app/controllers/JobsController/index.js
+++ src/server/app/controllers/JobsController/index.js
@@ -182,9 +182,18 @@ exports.createcontact = (req, res) => {
   }
 }
 
+// View a Job-contact
+exports.contactshow = (req, res) => {
+  const { UserId } = res.locals
+  models.Contact.findAll({where:{UserId:UserId,id:req.params.id}}).then((contact) => {
+    if (contact === null) {
+      res.status(404).json({ message: 'Contact Not Found.' })
+      return
+    }
 
+    res.status(200).json({ message: 'Contact Details Fetched.', contact })
+  }).catch((err) => {
+    res.status(500).json({ message: "Something Doesn't seem right" ,err:err})
+  })
+}
 
-
-
-
-  
